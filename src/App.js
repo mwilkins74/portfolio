172e8c015a5b720d8f05c5cd9acdccc1f18fa6f0
Mark <markwilkins86@gmail.com>
2023-10-11 16:47:44 -0400

    Footer Component

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
       <Nav />
       <About />
       <Experience />
-      <Services />
+      {/* <Services /> */}
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Footer.css'
+import { AiFillLinkedin, AiOutlineGithub } from "react-icons/ai";
+import { BsSpotify } from "react-icons/bs"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className="footer__logo">Mark Wilkins</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/mark-wilkins-84565666/" target="_blank" rel="noreferrer">
+          <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/mwilkins74" target="_blank" rel="noreferrer">
+          <AiOutlineGithub />
+        </a>
+        <a href="https://github.com/mwilkins74" target="_blank" rel="noreferrer">
+          <BsSpotify />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mark Wilkins Portfolio</small>
+      </div>
+    </footer>
   )
 }
 
